1cee4a03b0f20d025fb427276e815ac363f0499f
Egor Pugin <egor.pugin@gmail.com>
2024-06-16 16:50:41 +0300

    Update.

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,20 @@ using namespace clang;
 void print(auto &&x) {
     std::cout << x;
 }
-void println(auto &&x) {
-    print(x);
-    print("\n");
-}
 
 struct dump_tokens : PreprocessorFrontendAction {
-    std::string space;
+    enum class space_type {
+        start_of_file,
+        none,
+        hash,
+        space,
+        newline,
+    };
+    enum class prev_line_type {
+        no_prev_line,
+        normal,
+        pp,
+    };
 
     void ExecuteAction() override {
         Preprocessor &PP = getCompilerInstance().getPreprocessor();
@@ -25,61 +32,91 @@ struct dump_tokens : PreprocessorFrontendAction {
         Lexer RawLex(SM.getMainFileID(), FromFile, SM, PP.getLangOpts());
         RawLex.SetKeepWhitespaceMode(true);
 
-        Token RawTok;
-        RawLex.LexFromRawLexer(RawTok);
-        while (RawTok.isNot(tok::eof)) {
-            //PP.DumpToken(RawTok, true);
-            //DumpToken(PP, RawTok, true);
-            DumpTokenMy(PP, RawTok);
-            //llvm::errs() << "\n";
-            RawLex.LexFromRawLexer(RawTok);
-        }
-    }
-    void DumpTokenMy(Preprocessor &PP, const Token &Tok) {
-        if (Tok.isAnnotation()) {
-            return;
-        }
-        if (Tok.getKind() == clang::tok::TokenKind::comment) {
-            return;
-        }
-        if (Tok.getKind() == clang::tok::TokenKind::unknown) {
-            space += PP.getSpelling(Tok);
-            return;
-        }
-        if (!space.empty()) {
-            if (space.contains('\n')) {
-                print("\n");
-            } else {
-                print(" ");
+        Token t;
+        space_type space{};
+        prev_line_type prev_line{};
+        char prev_sym{};
+        auto print_newline = [&](char c) {
+            if (!prev_sym || prev_sym == '\n') {
+                prev_sym = c;
+                return;
             }
-            space.clear();
+            prev_sym = c;
+            print(c);
+        };
+        while (1) {
+            RawLex.LexFromRawLexer(t);
+            if (t.isAnnotation()) {
+                continue;
+            }
+            switch (t.getKind()) {
+            case tok::eof:
+                return;
+            case tok::comment:
+            case tok::unknown:
+                if (PP.getSpelling(t).contains('\n')) {
+                    if (prev_line == prev_line_type::pp) {
+                        print_newline('\n');
+                    }
+                    prev_line = prev_line_type::no_prev_line;
+                }
+                continue;
+            case tok::hash:
+                if (prev_line == prev_line_type::no_prev_line) {
+                    print_newline('\n');
+                    prev_line = prev_line_type::pp;
+                }
+            default:
+                if (prev_line == prev_line_type::no_prev_line) {
+                    prev_line = prev_line_type::normal;
+                }
+                print(PP.getSpelling(t));
+                print_newline(' ');
+                continue;
+            }
+
+            /*while (!t.is(tok::eof) && (false
+                || t.is(clang::tok::TokenKind::comment)
+                || t.is(clang::tok::TokenKind::unknown)
+                )) {
+                RawLex.LexFromRawLexer(t);
+            }
+            if (t.is(tok::eof)) {
+                break;
+            }
+            if (!t.getFlag(clang::Token::TokenFlags::StartOfLine)) {
+                throw std::runtime_error{"not a line start"};
+            }
+
+            prev_line = t.is(clang::tok::TokenKind::hash) ? prev_line_type::pp : prev_line_type::normal;
+
+            print(PP.getSpelling(t));
+            print(" ");
+            RawLex.LexFromRawLexer(t);
+
+            while (!t.getFlag(clang::Token::TokenFlags::StartOfLine) && !t.is(tok::eof)) {
+                if (false
+                    || t.is(clang::tok::TokenKind::comment)
+                    || t.is(clang::tok::TokenKind::unknown)
+                ) {
+                    RawLex.LexFromRawLexer(t);
+                    continue;
+                }
+                print(PP.getSpelling(t));
+                print(" ");
+                RawLex.LexFromRawLexer(t);
+            }
+            if (t.is(tok::eof)) {
+                break;
+            }
+            if (prev_line == prev_line_type::pp) {
+                print("\n");
+                prev_line = prev_line_type::no_prev_line;
+            }
+            print(PP.getSpelling(t));
+            print(" ");
+            RawLex.LexFromRawLexer(t);*/
         }
-        print(PP.getSpelling(Tok));
-    }
-    void DumpToken(Preprocessor &PP, const Token &Tok, bool DumpFlags) const {
-      llvm::errs() << tok::getTokenName(Tok.getKind());
-
-      if (!Tok.isAnnotation())
-        llvm::errs() << " '" << PP.getSpelling(Tok) << "'";
-
-      if (!DumpFlags) return;
-
-      llvm::errs() << "\t";
-      if (Tok.isAtStartOfLine())
-        llvm::errs() << " [StartOfLine]";
-      if (Tok.hasLeadingSpace())
-        llvm::errs() << " [LeadingSpace]";
-      if (Tok.isExpandDisabled())
-        llvm::errs() << " [ExpandDisabled]";
-      if (Tok.needsCleaning()) {
-        const char *Start = PP.getSourceManager().getCharacterData(Tok.getLocation());
-        llvm::errs() << " [UnClean='" << StringRef(Start, Tok.getLength())
-                     << "']";
-      }
-
-      llvm::errs() << "\tLoc=<";
-      PP.DumpLocation(Tok.getLocation());
-      llvm::errs() << ">";
     }
 };
 
